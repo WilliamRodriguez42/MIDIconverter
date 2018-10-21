8047df03f415601b2b5b08b1975ecc89b2ca5136
WilliamRodrigue42 <williamjr@ufl.edu>
2018-10-21 10:46:20 -0400

    Changed variable names

--- example/example.py
+++ example/example.py
@@ -2,7 +2,7 @@ import sys, os # You don't need these libraries for anything other than importin
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Add the parent directory so that we can import MIDIconverter
 #                                                                               This step usually isn't necessary
 
-from MIDIconverter import MConv
+from MIDIConverter import MConv
 
 # Plays Simple and Clean by Hikaru Utada
 # First read the midi, then write to text
@@ -23,7 +23,7 @@ a.S = (0.75, 0.25)
 a.read_midi('Clean.mid', wav=False) # Reads a midi and saves data to write to wav format (Writing to .wav can take a long time so disable this if you don't need it)
 a.write_text('Clean.txt') # Writes a text file that an AI can train on
 
-""" All possible write/play functions listed below """
+""" All possible write/play functions listed in the following comments """
 # a.write_wave('Clean.wav') # Writes a wave file
 # a.write_matlab('Clean.mat') # Writes a .mat file which can be read by matlab
 # a.play() # Plays the audio
